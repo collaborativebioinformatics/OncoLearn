3b2c398481c2e585b4cac613080791cf526ac96f
HalkScout <andrewscouten@gmail.com>
2026-01-08 00:47:04 -0500

    TCGA Lung cancer download
    Co-authored-by: Xinru Zhang

--- scripts/data/TCGA_Lung.R
+++ scripts/data/TCGA_Lung.R
@@ -1,20 +1,28 @@
-if (!require("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-
-BiocManager::install("TCGAbiolinks")
-
+library(tidyverse)
 library(TCGAbiolinks)
+library(GEOquery)
+library(SummarizedExperiment)
 
-#set path to download files
-#setwd()
+# Set output directory for lung cancer data
+data_dir <- "./data"
 
+# Create directory if it doesn't exist and set as working directory
+dir.create(data_dir, recursive = TRUE, showWarnings = FALSE)
+setwd(data_dir)
 
-# query tcga database
-query <- GDCquery(project = c("TCGA-LUSC","TCGA-MESO", "TCGA-LUAD"), 
+# Query TCGA database for lung cancer cohorts (LUSC, MESO, LUAD)
+query <- GDCquery(project = c("TCGA-LUSC", "TCGA-MESO", "TCGA-LUAD"),
                   data.category = "Transcriptome Profiling",
                   experimental.strategy = "RNA-Seq",
-                  data.type = "Gene Expression Quantification", data.format="TSV",
-                  workflow.type = "STAR - Counts") # or use UUIDs
+                  data.type = "Gene Expression Quantification",
+                  data.format = "TSV",
+                  workflow.type = "STAR - Counts")
 
-# download using API connection more reliably than gdcdata
+# Download data using API connection
 GDCdownload(query, method = "api", files.per.chunk = 30)
+
+# Prepare and save the RNA-Seq data
+output_filename <- "TCGA_Lung-RNASeq.RData"
+data <- GDCprepare(query = query,
+                   save.filename = output_filename,
+                   save = TRUE)
